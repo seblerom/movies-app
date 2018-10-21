328cc2a742bc08d2c4bf3b25378bca67f3c78755
seblerom <sleon@mailexternos.com.ar>
2018-10-20 23:38:12 -0300

    Adding Cocoapods - Alamofire and AlamofireImage

--- MoviesApp/MoviesApp/Features/List Movies/View Controllers/MAMoviesListViewController.swift
+++ MoviesApp/MoviesApp/Features/List Movies/View Controllers/MAMoviesListViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class MAMoviesListViewController: UIViewController {
 
-    lazy var searchController :UISearchController = {
-       let searchController = UISearchController(searchResultsController: nil)
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search Movies"
-        navigationItem.searchController = searchController
-        searchController.searchResultsUpdater = self
-        definesPresentationContext = true
-        return searchController
-    }()
+//    lazy var searchController :UISearchController = {
+//       let searchController = UISearchController(searchResultsController: nil)
+//        searchController.obscuresBackgroundDuringPresentation = false
+//        searchController.searchBar.placeholder = "Search Movies"
+//        navigationItem.searchController = searchController
+//        searchController.searchResultsUpdater = self
+//        definesPresentationContext = true
+//        return searchController
+//    }()
     
     lazy var collectionView : UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -30,16 +30,20 @@ class MAMoviesListViewController: UIViewController {
         return collection
     }()
     
+    override func loadView() {
+        super.loadView()
+        collectionView.register(MAMovieListCellCollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .black
         title = "Movies"
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.navigationBar.barTintColor = .black
-        navigationController?.navigationBar.tintColor = .white
+//        navigationController?.navigationBar.prefersLargeTitles = true
+//        navigationController?.navigationBar.barTintColor = .black
+//        navigationController?.navigationBar.tintColor = .white
         addCollectionViewConstraints()
-        collectionView.register(MAMovieListCellCollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        
     }
     
     private func addCollectionViewConstraints() {
@@ -58,7 +62,7 @@ class MAMoviesListViewController: UIViewController {
 //MARK: - UICollectionViewDataSource
 extension MAMoviesListViewController : UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -72,7 +76,10 @@ extension MAMoviesListViewController : UICollectionViewDataSource {
 extension MAMoviesListViewController : UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width / 2, height: 200)
+        
+        let width = collectionView.frame.width / 2
+        let height = width / CGFloat(MAConstants.MAAspectRatio.posterAspectRatio)
+        return CGSize(width: width, height: width / height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
@@ -86,10 +93,10 @@ extension MAMoviesListViewController : UICollectionViewDelegate, UICollectionVie
 }
 
 //MARK: - UISearchResultsUpdating
-extension MAMoviesListViewController : UISearchResultsUpdating {
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        
-    }
-
-}
+//extension MAMoviesListViewController : UISearchResultsUpdating {
+//    
+//    func updateSearchResults(for searchController: UISearchController) {
+//        
+//    }
+//
+//}
